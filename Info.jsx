226e5ba82b026721d627c066763d16c1b50d7b20
Vasudhachoudhary <86521752+Vasudhachoudhary@users.noreply.github.com>
2021-06-27 08:47:09 -0700

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1>here we will learn about javascript</h1>
+      <p>
+        Bootcamps are fun as here we get to know new people and learn as well as
+        implement whatever we learnt{" "}
+      </p>
+    </div>
+  );
+}
+export default Info;
